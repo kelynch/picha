451098054cba6c23d7580092e4113f8a1f585554
Chad Nelson <chadbnelson@gmail.com>
2015-09-29 16:23:50 -0400

    Update controller + initializer to handle multiple omeka instances.
    Update name of omeka config file to reflect that it contains site
    information.

--- app/controllers/omeka_image_controller.rb
+++ app/controllers/omeka_image_controller.rb
@@ -1,9 +1,9 @@
 require 'omeka_client'
 
 class OmekaImageController < ApplicationController
-  def show
+  def show |site_name|
     # TODO: Use the Riiif url helper to construct the iiif-friendly image URL show here
-    omeka_site = OmekaConfig.config[:omeka_site]
-    omeka_api_key = OmekaConfig.config[:omeka_api_key]
+    omeka_site = OmekaConfig.config[:site_name][:url]
+    omeka_api_key = OmekaConfig.config[:site_name][:api_key]
   end
 end
