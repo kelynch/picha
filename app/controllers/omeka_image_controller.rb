d887214140088b7d3614c434dbcb2db4e0aca913
Katherine Lynch <katherine.lynch@temple.edu>
2015-09-29 15:36:58 -0400

    Example dead simple omeka.yml file, skeletal URL controller

---
+++ app/controllers/omeka_image_controller.rb
@@ -0,0 +1,7 @@
+require 'omeka_client'
+
+class OmekaImageController < ApplicationController
+  def show
+    # TODO: Use the Riiif url helper to construct the iiif-friendly image URL show here
+  end
+end
