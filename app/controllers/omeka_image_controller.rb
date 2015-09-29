3319aa86c289812fae71cbede5510a9af358c233
Chad Nelson <chadbnelson@gmail.com>
2015-09-29 16:31:35 -0400

    Pass site_name param correctly

--- app/controllers/omeka_image_controller.rb
+++ app/controllers/omeka_image_controller.rb
@@ -1,7 +1,7 @@
 require 'omeka_client'
 
 class OmekaImageController < ApplicationController
-  def show |site_name|
+  def show |:site_name|
     # TODO: Use the Riiif url helper to construct the iiif-friendly image URL show here
     omeka_site = OmekaConfig.config[:site_name][:url]
     omeka_api_key = OmekaConfig.config[:site_name][:api_key]
