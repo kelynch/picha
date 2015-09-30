0f6da05c9cdd1e2ba57b34e25bed6a5f0797999d
Katherine Lynch <katherine.lynch@temple.edu>
2015-09-30 06:58:45 -0400

    Break out omeka site alias into parameter

--- lib/omeka_utils.rb
+++ lib/omeka_utils.rb
@@ -2,19 +2,23 @@ require "omeka_client"
 
 module OmekaUtils
 
-  @omeka_site = OmekaConfig.config[:omeka_a11y][:url]
-  @omeka_api_key = OmekaConfig.config[:omeka_a11y][:api_key]
-
-  @client = OmekaClient::Client.new(@omeka_site, @omeka_api_key)
-
-  def basic_asset_info(item_id)
+  def basic_asset_info(omeka_site_alias, item_id)
     # Not much yet -- pulling in item identifer, more can be added
     asset_info_hash = Hash.new
-
-    item = @client.get_item(item_id)
+    client = make_client(omeka_site_alias)
+    item = client.get_item(item_id)
     asset_info_hash[:identifier] = item.data.id if item.data.public
     return asset_info_hash
   end
   module_function :basic_asset_info
+
+  protected
+  def self.make_client(omeka_site_alias)
+    binding.pry
+    omeka_site = OmekaConfig.config[omeka_site_alias.to_sym][:url]
+    omeka_api_key = OmekaConfig.config[omeka_site_alias.to_sym][:api_key]
+    client = OmekaClient::Client.new(omeka_site, omeka_api_key)
+    client
+  end
 
 end
