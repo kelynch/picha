017b21a4be3373de34e904a65f38af4bf326048b
Katherine Lynch <katherine.lynch@temple.edu>
2015-09-30 09:58:06 -0400

    Removed redundant engine mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  mount Riiif::Engine => '/riiif'
+  
   iiif_for 'riiif/image', at: '/riiif'
 
   # You can have the root of your site routed with "root"
