2b97ffb60d39be8ef79b132fc06462b8138f3481
Katherine Lynch <katherine.lynch@temple.edu>
2015-09-29 20:37:24 -0400

    Restructured asset info manifest as hash (teeny tiny additions so far, more needed)

--- lib/omeka_utils.rb
+++ lib/omeka_utils.rb
@@ -7,8 +7,13 @@ module OmekaUtils
 
   @client = OmekaClient::Client.new(@omeka_site, @omeka_api_key)
 
-  def basic_asset_info()
-    binding.pry()
+  def basic_asset_info(item_id)
+    # Not much yet -- pulling in item identifer, more can be added
+    asset_info_hash = Hash.new
+
+    item = @client.get_item(item_id)
+    asset_info_hash[:identifier] = item.data.id if item.data.public
+    return asset_info_hash
   end
   module_function :basic_asset_info
 
