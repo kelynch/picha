227d075ca810d874bec80a3b885d12f8c12e6ccf
Katherine Lynch <katherine.lynch@temple.edu>
2015-10-01 09:40:19 -0400

    Added id pointer, rebundled Gemfile

--- app/controllers/omeka_image_controller.rb
+++ app/controllers/omeka_image_controller.rb
@@ -5,12 +5,12 @@ require 'Riiif'
 class OmekaImageController < ApplicationController
   def showother(sitename, identifier)
     # TODO: Use the Riiif url helper to construct the iiif-friendly image URL show here
-    omeka_site = OmekaConfig.config[sitename][url]
+    omeka_site = OmekaConfig.config[:sitename][:url]
     omeka_api_key = OmekaConfig.config[:sitename][:api_key]
   end
 
   def show
     image_info  = OmekaUtils.basic_asset_info(params[:sitename], params[:identifier])
-    @iiif_image = Riiif::Engine.routes.url_helpers.info_path(params[:id])
+    @iiif_image = Riiif::Engine.routes.url_helpers.info_path(params[:identifier])
   end
 end
